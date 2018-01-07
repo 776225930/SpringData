db95df90b12d2979cd2a4942ec1e07ee6c563a0c
Jiang <776225930@qq.com>
2018-01-07 13:13:26 +0800

    配置好数据源,测试一下

---
+++ src/com/example/springdata/test/SpringDataTest.java
@@ -0,0 +1,14 @@
+package com.example.springdata.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SpringDataTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
