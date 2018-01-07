54ea9a1acd25573b135e8284f962780afc7cb476
Jiang <776225930@qq.com>
2018-01-07 15:24:41 +0800

    在PersonRepsotory接口中使用关键字写方法,并测试OK

--- src/com/example/springdata/PersonRepository.java
+++ src/com/example/springdata/PersonRepository.java
@@ -1,8 +1,35 @@
 package com.example.springdata;
 
-import org.springframework.data.repository.Repository;
+import java.util.Date;
+import java.util.List;
 
-public interface PersonRepository extends Repository<Person, Integer>{
+import org.springframework.data.repository.Repository;
+/**
+ * 1.Repository是一个空接口,即是一个标记接口
+ * 2.若自己定义的接口继承了Repository,则该接口会被IOC容器识别为一个 Repository Bean.
+ * 纳入到IOC容器中.进而可以在该接口中定义满足一定规范的方法
+ * 3.实际上也可以通过注解@RepositoryDefinition来代替继承Repository接口
+ * 在 Repository 子接口中声明方法
+ * 1. 不是随便声明的. 而需要符合一定的规范
+ * 2. 查询方法以 find | read | get 开头
+ * 3. 涉及条件查询时，条件的属性用条件关键字连接
+ * 4. 要注意的是：条件属性以首字母大写。
+ * 5. 支持属性的级联查询. 若当前类有符合条件的属性, 则优先使用, 而不使用级联属性. 
+ * 若需要使用级联属性, 则属性之间使用 _ 进行连接. 
+ * @author JHao
+ *
+ */
+//@RepositoryDefinition(domainClass=Person.class,idClass=Integer.class)
+public interface PersonRepository extends Repository<Person, Integer> {
 	//根据 lastName 来获取对应的 Person
 	Person getByLastName(String lastName);
+	
+	//WHERE lastName LIKE ?% AND id<?
+	List<Person> getByLastNameStartingWithAndIdLessThan(String lastName,Integer id);
+	
+	//WHERE lastName LIKE %? AND id<?
+	List<Person> getByLastNameEndingWithAndIdLessThan(String lastName,Integer id);
+	
+	//WHERE email IN (?,?,?) OR birth<?
+	List<Person> getByEmailInOrBirthLessThan(List<String> emails,Date birth);
 }
--- src/com/example/springdata/test/SpringDataTest.java
+++ src/com/example/springdata/test/SpringDataTest.java
@@ -3,6 +3,9 @@ package com.example.springdata.test;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -15,8 +18,10 @@ import com.example.springdata.PersonRepository;
 
 public class SpringDataTest {
    private ApplicationContext ctx;
+   private PersonRepository personRepository;
    {
 	   ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+	   personRepository=ctx.getBean(PersonRepository.class);
    }
 	@Test
 	public void test() throws SQLException {
@@ -29,10 +34,20 @@ public class SpringDataTest {
 	
 	@Test
 	public void testHelloWorldSpringData(){
-		PersonRepository personRepository=ctx.getBean(PersonRepository.class);
+		System.out.println(personRepository.getClass().getName());
 	    Person person=personRepository.getByLastName("AA");
 	    System.out.println(person);
-	
+	}
+	@Test
+	public void testKeyWords(){
+		List<Person> persons=personRepository.getByLastNameStartingWithAndIdLessThan("A", 3);
+	    System.out.println("getByLastNameStartingWithAndIdLessThan: "+persons);
+	    
+	    persons=personRepository.getByLastNameEndingWithAndIdLessThan("A", 3);
+	    System.out.println("getByLastNameEndingWithAndIdLessThan: "+persons);
+	    
+	    persons=personRepository.getByEmailInOrBirthLessThan(Arrays.asList("aa@163.com","bb@163.com"), new Date());
+	    System.out.println("getByEmailInOrBirthLessThan: "+persons);
 	}
 	
 	
